b2298dcb3e93ea956d75ac7b526bbbefcae0ad5c
Purab07 <73265862+Purab07@users.noreply.github.com>
2021-03-13 17:31:28 -0800

    Update paperball.js

--- paperball.js
+++ paperball.js
@@ -3,7 +3,7 @@ class Paperball{
         var options={
             isStatic: false,
             restitution: 0.3,
-            friction: 0.5,
+            friction: 1.0,
             density: 1.2
         }
         this.body = Bodies.circle(x,y,radius,options);
