7d60e393275859d56301560af2c0376b20e9c2db
Purab07 <73265862+Purab07@users.noreply.github.com>
2021-04-27 19:38:44 -0700

    Update paperball.js

--- paperball.js
+++ paperball.js
@@ -6,7 +6,7 @@ class Paperball{
             friction: 1.0,
             density: 1.2
         }
-        this.image = loadImage('images/paper.png');
+        this.image = loadImage('paper.png');
         this.body = Bodies.circle(x,y,radius,options);
         this.radius = radius;
         World.add(world,this.body)
